c31d230acfad59dbd565ffe0edc4c90077bffa85
Samhita-sopho <samhitak@hotmail.com>
2018-08-12 14:46:57 +0530

    Update bty.py

--- bty.py
+++ bty.py
@@ -1,28 +1,58 @@
 from bs4 import BeautifulSoup
+from operator import itemgetter
 import re
 import requests
 
 class TopProjects:
      def __init__(self,url):
           self.url=url
-          self.r=requests.get(self.url)
-          self.text=self.r.text
+          self.r=requests.get(self.url,verify=True)
+          #print(self.r)
+          #print(self.r.content)
+          self.text=self.r.content
           self.projects=[]
           '''getTopProjects(self)
           printTopProjects(self)'''
+     def convertToNum(self,text):
+          strnum=''
+          for i in text:
+               if i.isdigit():
+                    strnum+=i;
+          return int(strnum)
 
      def getTopProjects(self):
           print("trying to get")
           soup=BeautifulSoup(self.text,"html.parser")
-          allProjects=soup.find_all("li",attrs={"class": "col-12 d-block width-full py-4 border-bottom public source"})
+          allProjects=soup.find_all("li",attrs={"class": "col-12 d-block width-full py-4 border-bottom public fork"})
           noOfProjects=len(allProjects)
           for i in range(0,noOfProjects):
               thisProject={}
+              thisProject['Contributors']=[]
               thisProject['ProjectName']=allProjects[i].div.h3.a.text
               atags=allProjects[i].find("div",attrs={"class":'f6 text-gray mt-2'}).find_all("a")
-              thisProject['Forks']=atags[1].text
+              thisProject['Forks']=self.convertToNum(atags[1].text)#int(filter(str.isdigit, atags[1].text))
+              thisProject['Link']=allProjects[i].div.h3.find("a")['href']
+              thisProject['Link']="http://www.github.com"+thisProject['Link']
+              projRequest=requests.get(thisProject['Link'])
+             # print(projRequest)
+              projContent=projRequest.content
+              subSoup=BeautifulSoup(projContent,"html.parser")
+              contributeLink=subSoup.find("div",attrs={"overall-summary overall-summary-bottomless"})
+              #print(contributeLink)
+              li_tag=contributeLink.find_all("li")
+              c_link=li_tag[3].find("a")['href']
+              c_link="http://www.github.com"+c_link
+              projRequest=requests.get(c_link)
+              print("the thrid request")
+              print(projRequest)
+              projContent=projRequest.content
+              subSoup=BeautifulSoup(projContent,"html.parser")
+              print(subSoup.find("div",attrs={"class":"graphs"}))
               #add the redirect link here to get the committees
               self.projects.append(thisProject)
+          l=sorted(self.projects,key=itemgetter('Forks'),reverse=True)
+          self.projects=l[0:5]
+          
 
      def printTopProjects(self):
           total=len(self.projects)
@@ -30,6 +60,7 @@ class TopProjects:
           for i in range(0,total):
                print(self.projects[i]['ProjectName'])
                print(self.projects[i]['Forks'])
+               print(self.projects[i]['Link'])
                print('*****************')
           
           
