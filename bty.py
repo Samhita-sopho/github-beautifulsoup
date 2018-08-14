e3da8c436d56a755480fcfa63334cad0a6e71bab
Samhita-sopho <samhitak@hotmail.com>
2018-08-14 23:06:15 +0530

    Update bty.py

--- bty.py
+++ bty.py
@@ -10,10 +10,15 @@ class TopProjects:
           self.org=org
           self.url="https://github.com/"+url+"?utf8=%E2%9C%93&q=&type=fork&language="
           self.r=requests.get(self.url,verify=True)
-          print(self.r)
-          #print(self.r.content)
-          self.text=self.r.content
-          self.projects=[]
+          print(self.r.status_code)
+          if self.r.status_code==200:
+               self.error_message="Clear"
+          else:
+               self.error_message="Not Available"
+          if self.error_message=="Clear":
+               self.text=self.r.content
+               self.projects=[]
+          
          
      '''The following convertToNum function return the number in the given text - this was necessary to
                                                                                                                                                  get the number of forks and commits only '''     
@@ -27,39 +32,46 @@ class TopProjects:
                projects are obtained and then they are sorted based on the number of forks'''
      def getTopProjects(self):
           print("trying to get")
-          soup=BeautifulSoup(self.text,"html.parser")
-          allProjects=soup.find_all("li",attrs={"class": "col-12 d-block width-full py-4 border-bottom public fork"}) #extracts all the projects
-          
-          noOfProjects=len(allProjects)
-          print(noOfProjects)
-          for i in range(0,noOfProjects):
-               
-               thisProject={}
-               thisProject['Contributors']=[]
-               thisProject['ProjectName']=str(allProjects[i].div.h3.a.text).strip()
-               atags=allProjects[i].find("div",attrs={"class":'f6 text-gray mt-2'}).find_all("a")
-               thisProject['Forks']=self.convertToNum(atags[1].text)
-               thisProject['Link']=allProjects[i].div.h3.find("a")['href']
-               thisProject['Link']="https://api.github.com/repos/"+self.org+"/"+thisProject['ProjectName']+"/contributors"       #link to access the contritors page       
-               print(thisProject['Link'])
-               
-               projRequest=requests.get(thisProject['Link'],verify=True)              
-               dictContrib=projRequest.json()
-               for j in range(0,len(dictContrib)):
-                    tempD={}
-                    tempD['cname']=dictContrib[j]["login"]
-                    tempD['ccount']=dictContrib[j]["contributions"]
-                    thisProject['Contributors'].append(tempD)
-               tempDict=sorted(thisProject['Contributors'],key=itemgetter('ccount'),reverse=True)
-               thisProject['Contributors']=tempDict[0:3]
+          if self.error_message=="Not Available":
+               return
+          if self.error_message=="Clear":
+               soup=BeautifulSoup(self.text,"html.parser")
+               allProjects=soup.find_all("li",attrs={"class": "col-12 d-block width-full py-4 border-bottom public fork"}) #extracts all the projects
+               noOfProjects=len(allProjects)
+               print(noOfProjects)
+               for i in range(0,noOfProjects):
+                    thisProject={}
+                    thisProject['Contributors']=[]
+                    thisProject['ProjectName']=str(allProjects[i].div.h3.a.text).strip()
+                    atags=allProjects[i].find("div",attrs={"class":'f6 text-gray mt-2'}).find_all("a")
+                    thisProject['Forks']=self.convertToNum(atags[1].text)
+                    thisProject['Link']=allProjects[i].div.h3.find("a")['href']
+                    thisProject['Link']="https://api.github.com/repos/"+self.org+"/"+thisProject['ProjectName']+"/contributors"       #link to access the contritors page
+                    print(thisProject['Link'])
+                    projRequest=requests.get(thisProject['Link'],verify=True)
+                    dictContrib=projRequest.json()
+                    print(dictContrib)
+                    for j in range(0,len(dictContrib)):
+                         tempD={"cname":"","ccount":0}
+                         print(dictContrib[j]["login"])
+                         print(dictContrib[j]["contributions"])
+                         tempD["cname"]=dictContrib[j]["login"]
+                         tempD["ccount"]=dictContrib[j]["contributions"]
+                         thisProject['Contributors'].append(tempD)
+                    tempDict=sorted(thisProject['Contributors'],key=itemgetter('ccount'),reverse=True)
+                    thisProject['Contributors']=tempDict[0:3]
                self.projects.append(thisProject)
-          l=sorted(self.projects,key=itemgetter('Forks'),reverse=True)#sorts the projects according to forks
-          self.projects=l[0:5]#obtains the top most projects by forks
+               l=sorted(self.projects,key=itemgetter('Forks'),reverse=True)#sorts the projects according to forks
+               self.projects=l[0:5]#obtains the top most projects by forks
           
 
      def printTopProjects(self):
-          total=len(self.projects)
+          if self.error_message=="Not Available":
+               print("No projects available to print")
+               return 
           print("trying to print")
+          total=len(self.projects)
+          print("Total Projects"+str(total))
           for i in range(0,total):
                print(self.projects[i]['ProjectName'])
                print(self.projects[i]['Forks'])
@@ -67,6 +79,7 @@ class TopProjects:
                print("Top committes")
                for j in range(0,len(self.projects[i]['Contributors'])):
                     print("NAME:"+self.projects[i]['Contributors'][j]["cname"]+"-->"+"COMMITS:"+str(self.projects[i]['Contributors'][j]["ccount"]))
+          print("*****************")
 
 org = raw_input("Enter an organization's name to extract from: ")
 
